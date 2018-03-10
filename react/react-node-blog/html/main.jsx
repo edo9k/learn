df85f7494113709a4011144c94368951535bd571
Eduardo França <edox86@gmail.com>
2018-03-10 17:09:49 -0300

    Routes and User model created, but with MongoDB errors.

--- react/react-node-blog/html/main.jsx
+++ react/react-node-blog/html/main.jsx
@@ -61,6 +61,47 @@ class Signin extends React.Component{
 }
 
 class Signup extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.handleNameChange     = this.handleNameChange.bind(this);
+    this.handleEmailChange    = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+
+    this.state = {
+      name:     '',
+      email:    '',
+      password: ''
+    }
+  }
+
+  handleNameChange(e){
+    this.setState({name:e.target.value})
+  }
+
+  handleEmailChange(e){
+    this.setState({email:e.target.value})
+  }
+
+  handlePasswordChange(e){
+    this.setState({password:e.target.value})
+  }
+
+  signUp(){
+    axios.post('/signup', {
+      name    : this.state.name,
+      email   : this.state.email,
+      password: this.state.password
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+
   render() {
     return (
       <div>
--- react/react-node-blog/readme.md
+++ react/react-node-blog/readme.md
@@ -2,6 +2,7 @@
 
 I'm using this tutorial: https://code.tutsplus.com/tutorials/creating-a-blogging-app-using-react-user-sign-in--cms-28568
 ~~But it is quite buggy (and it does not seem to be the way React is used nowadays).~~
+The old problems have disappeared, but now in part two MongoDB is acting up. ¯\_(ツ)_/¯ 
 
 
 
