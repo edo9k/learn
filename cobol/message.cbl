d23f18ef80e4ffdc7b1953b1591a651812fc9908
Eduardo França <edox86@gmail.com>
2018-03-11 00:45:09 -0300

    studying some basic cobol examples

---
+++ cobol/message.cbl
@@ -0,0 +1,15 @@
+       IDENTIFICATION DIVISION.
+       AUTHOR. edo9k.
+       PROGRAM-ID. PreDefMsg. 
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 PreDefMsg PIC X(21) VALUE 'Hello World of Cobol!'.
+
+       PROCEDURE DIVISION.
+       DISPLAY PreDefMsg.
+       STOP RUN.
+
+
+       
+
