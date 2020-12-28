8d4c7f43818ba7d8a1ac5065b91f244c7f4b08e9
Eduardo França <edox86@gmail.com>
2020-12-28 12:11:11 -0500

    Haskell Course: Fixed the Hangman example
    Turns out it's really hard to catch elementary mistakes when you're
    dealing with a language you have virtually zero experience with.

--- haskell/hangman.hs
+++ haskell/hangman.hs
@@ -16,14 +16,15 @@ turn word display n =
         then putStrLn "You win!"
         else mkguess word display n 
 
+mkguess :: String -> String -> Int -> IO () 
 mkguess word display n =
   do putStrLn (display ++ " " ++ take n (repeat '*'))
-    putStr "  Enter your guess:  "
-    q <- getLine
-    let (correct, display') = check word display (q!!0)
-    let n' = if correct then n else n-1
-    turn word display' n'
+     putStr " Enter your guess: "
+     q <- getLine
+     let (correct, display') = check word display (q!!0)
+     let n' = if correct then n else n-1
+     turn word display' n'
 
 starman :: String -> Int -> IO()
-starman word n = turn word = ['-' | x <- word] n
+starman word n = turn word ['-' | x <- word] n
 
