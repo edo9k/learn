2ba2352d4f94bbcaae683133ee39f010649e0342
Eduardo França <edox86@gmail.com>
2021-01-27 23:40:44 -0500

    Python tiny machine learining
    Up to section 5.3 "Build Models"

--- python/tiny-ml/main.py
+++ python/tiny-ml/main.py
@@ -37,3 +37,39 @@ print(dataset.groupby('class').size())
 # box and whisker plots
 dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 pyplot.show()
+
+# histograms
+dataset.hist()
+pyplot.show()
+
+# scatter plot matrix 
+scatter_matrix(dataset)
+pyplot.show()
+
+
+# split validation dataset 
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+# spot check algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.appned(('SVM', SVC(gamma='auto')))
+
+# evaluate each model
+results = []
+names = []
+
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, X_train, Y_train, scoring='accuracy')
+    results.append(cv_results)
+    names.apend(name)
+    print('%s: %f (%f)', % (name, cv_results.mean(), cv_results.std()))
+
