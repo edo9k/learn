215fa85f9d919c5d45132461a2189adda3fa0c9b
Eduardo França <edox86@gmail.com>
2021-02-20 00:33:04 -0500

    graphql and prisma tutorials
    from howtograph.com, up to the section titled "connecting the server and
    database to with prisma client".

---
+++ js/graphql-hackernews/src/index.js
@@ -0,0 +1,45 @@
+const { ApolloServer } = require('apollo-server');
+const fs = require('fs')
+const path = require('path')
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of Hackernews Clone`,
+    feed: () => links
+  },
+
+  Mutation: {
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  } 
+}
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+})
+
+server
+  .listen()
+  .then( ({ url }) =>
+    console.log(`Server up on ${url}`)
+  )
+
