377c92745750831b4fefaac7e6b5a214f508648e
Eduardo França <edox86@gmail.com>
2021-02-26 22:26:48 -0500

    react-redux introduction tutorial

---
+++ react/redux-tutorial/src/TodoApp.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
+import VisibilityFilters from './components/VisibilityFilters'
+import './styles.css'
+
+export default function TodoAdd() {
+  return (
+    <div className='todo-app'>
+      <h1>Todo List</h1>
+      <AddTodo />
+      <TodoList />
+      <VisibilityFilters />
+    </div>
+  )
+}
+
