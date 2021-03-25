bebdc355589b42173e7c5b1bb4a7936eccf7b117
Eduardo França <edox86@gmail.com>
2021-03-24 22:22:25 -0400

    angular - tour of heroes
    Angular introduction: installing angular, creating a new project, the
    hero editor and displaying a list.

---
+++ angular/tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,18 @@
+import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Ada Lovelace'      },
+  { id: 12, name: 'Alan Turing'       },
+  { id: 13, name: 'Brian Kerninghan'  },  
+  { id: 14, name: 'Dennis Ritchie'    },
+  { id: 15, name: 'Grace Hopper'      },
+  { id: 16, name: 'Ken Thompson'      },
+  { id: 17, name: 'Mary Keller'       },
+  { id: 18, name: 'Niklaus Wirth'     },
+  { id: 19, name: 'Sophie Wilson'     },
+  { id: 20, name: 'Charles Babbage'   },
+  { id: 21, name: 'Curt Herztark '    },
+  { id: 22, name: 'Konrad Zuze'       }, 
+  { id: 23, name: 'Joseph Jacquard'   },
+]
+  
