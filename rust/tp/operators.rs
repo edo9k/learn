18b278e2d48aea600314d373309147dff8172869
Eduardo França <edox86@gmail.com>
2021-04-15 22:51:30 -0400

    rust - operators

---
+++ rust/tp/operators.rs
@@ -0,0 +1,17 @@
+
+fn main() {
+    println!("Operators work as expected.");
+    println!("1 + 2 = {}", 1 + 2);
+    println!("2 - 2 = {}", 2 - 2);
+    println!("2 * 4 = {}", 2 * 4);
+    println!("4 / 2 = {}", 4 / 2);
+    println!("5 % 2 = {}", 5 % 2);
+
+    println!("
+    same for comparison operator: < > <= >= == !=
+    and for logical operators: && || ! 
+    also bitwise operators: & | ^ ! << >> >>>
+    ");
+
+}
+
