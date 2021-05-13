96ffea2a11151a330e0b989af7f65376010318a6
Eduardo França <edox86@gmail.com>
2021-05-13 01:34:58 -0300

    vue - 4noobs tutorial

---
+++ vue/4noobs/src/components/Hi.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1> 
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Hi',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+h1 {
+  margin: 1px solid red;
+}
+</style>
