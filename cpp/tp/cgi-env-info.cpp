85161ce0ff515acb78b9a3b69c4919e3db8cb53f
Eduardo França <edox86@gmail.com>
2021-07-31 22:48:42 -0400

    c++ - cgi hello world
    Program that, acting through the common gateway interface, dumps all
    server env variables when accessed through a browser.

---
+++ cpp/tp/cgi-env-info.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+const string ENV[24] = {
+  "COMSPEC",              "DOCUMENT_ROOT",        "GATEWAY_INTERFACE",    
+  "HTTP_ACCEPT",          "HTTP_ACCEPT_ENCODING", "HTTP_ACCEPT_LANGUAGE", 
+  "HTTP_CONNECTION",      "HTT_HOST",             "HTTP_USER_AGENT", "PATH",
+  "QUERY_STRING",         "REMOTE_ADDR",          "REMOTE_PORT",
+  "REQUEST_METHOD",       "REQUEST_URI",          "SCRIPT_FILENAME",
+  "SCRIPT_NAME",          "SERVER_ADDR",          "SERVER_ADMIN",
+  "SERVER_NAME",          "SERVER_PORT",          "SERVER_PROTOCOL",
+  "SERVER_SIGNATURE",     "SERVER_SOFTWARE"
+};
+
+int main () {
+  cout  << "Content-type:text/html\r\n\r\n" 
+        << "<html>\n"
+        << "<head>\n"
+        << "<title>CGI Env Dump</title>\n"
+        << "</head>\n"
+        << "<body>\n"
+        << "<table border=0 cellspacing=2>";
+
+  for (int i = 0; i < 24; i++) {
+    cout << "<tr><td>" << ENV[ i ] << "</td><td>";
+
+    // get environment variable
+    char *value = getenv( ENV[ i ].c_str() );
+
+    if (value != 0) cout << value;
+    else cout << "<undefined>";
+
+    cout << "</td></tr>\n";
+  }
+
+  cout << "</table>\n</body>\n</html>\n";
+
+  return 0;
+}
