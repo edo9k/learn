f5ace87ac189eb499f18aab2854dc27dee872432
Eduardo França <edox86@gmail.com>
2021-10-11 03:12:10 -0300

    fortran - basic syntax

---
+++ fortran/tp/basic-syntax.f90
@@ -0,0 +1,19 @@
+program addNumbers
+! turn off implicit typing
+implicit none
+
+! declarations
+real :: a, b, result
+
+! procedure division
+a = 0.03 
+b = 0.01
+
+result = a - b 
+
+print *, 'The total is ', result
+
+end program addNumbers
+
+
+
