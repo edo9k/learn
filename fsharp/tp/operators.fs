7d0582761a380a7ab545997db5f2b5899a3bee05
Eduardo França <edox86@gmail.com>
2022-05-01 23:46:54 -0300

    f# - arithmetic operators

---
+++ fsharp/tp/operators.fs
@@ -0,0 +1,13 @@
+(* arithmetic, boolean and binary operators *)
+
+let a: int32 = 21
+let b: int32 = 10
+
+printfn "~ arithmetic operators ~"
+printfn "a + b = %d" (a + b)
+printfn "a - b = %d" (a - b)
+printfn "a * b = %d" (a * b)
+printfn "a / b = %d" (a / b)
+printfn "a %% b = %d" (a % b)
+printfn "a ** b = %g" ((float a) ** (float b))
+
