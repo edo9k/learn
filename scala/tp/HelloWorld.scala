849c53b86fda362fc0556642860a88e7a692dff1
Eduardo França <edox86@gmail.com>
2022-09-30 22:38:16 -0300

    scala - hello world

---
+++ scala/tp/HelloWorld.scala
@@ -0,0 +1,12 @@
+
+// a bit shorter than the java version
+// and no need to littering semicolons all over 
+// the place.
+
+object HelloWorld {
+
+  def main(args: Array[String]) {
+    println("Hello, World! (from Scala)")
+  }
+}
+
