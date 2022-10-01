bf173dcd31445e36e5dd6c2c1a558a41c6096758
Eduardo França <edox86@gmail.com>
2022-09-30 23:00:23 -0300

    scala - variable assignment

---
+++ scala/tp/Variables.scala
@@ -0,0 +1,14 @@
+
+object Variables {
+  def main(args: Array[String]) {
+    val name :String = "Eduardo França"
+    val year :Int = 2022
+
+    val noExplicitType = "Scala usually can guess the type."
+
+    println(name)
+    println(year)
+    println(noExplicitType)
+  }
+}
+
