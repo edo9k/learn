a0aeceece8346e76158d710561ec460154a11ffb
Eduardo França <edox86@gmail.com>
2023-01-25 23:23:37 -0300

    c# - ms graph tutorial

---
+++ csharp/GraphAppOnlyTutorial/GraphHelper.cs
@@ -0,0 +1,52 @@
+
+using Azure.Core;
+using Azure.Identity;
+using Microsoft.Graph;
+
+class GraphHelper
+{
+  // Settings object
+  private static Settings? _settings;
+  // App-ony auth token credential
+  private static ClientSecretCredential? _clientSecretCredential;
+  // Client configured with app-only authentication
+  private static GraphServiceClient? _appClient;
+
+  public static void InitializeGraphForAppOnlyAuth(Settings settings)
+  {
+    _settings = settings;
+
+    // Ensure settings isn't null
+    _ = settings ??
+      throw new System.NullReferenceException("Settings cannot be null");
+
+    _settings = settings;
+
+    if (_clientSecretCredential == null)
+    {
+      _clientSecretCredential = new ClientSecretCredential(
+          _settings.TenantId, _settings.ClientId, _settings.ClientSecret);
+    }
+
+    if (_appClient == null)
+    {
+      _appClient = new GraphServiceClient(_clientSecretCredential,
+          // Use the default scope, which will request the scopes
+          // configured on the app registration
+          new[] {"https://graph.microsoft.com/.default"});
+    }
+  }
+
+  public static async Task<string> GetAppOnlyTokenAsync()
+  {
+    // Ensure credential isn't null
+    _ = _clientSecretCredential ??
+      throw new System.NullReferenceException("Graph has not been initialized for app-only auth");
+
+    // Request token with given scopes
+    var context = new TokenRequestContext(new[] {"https://graph.microsoft.com/.default"});
+    var response = await _clientSecretCredential.GetTokenAsync(context);
+    return response.Token;
+  }
+
+}
---
+++ csharp/GraphAppOnlyTutorial/Program.cs
@@ -0,0 +1,81 @@
+﻿
+Console.WriteLine(".NET Graph App-only Tutorial\n");
+
+var settings = Settings.LoadSettings();
+
+// Initialize Graph
+InitializeGraph(settings);
+
+int choice = -1;
+
+while (choice != 0)
+{
+  Console.WriteLine("Please choose one of the following options:");
+  Console.WriteLine("0. Exit");
+  Console.WriteLine("1. Display access token");
+  Console.WriteLine("2. List users");
+  Console.WriteLine("3. Make a Graph call");
+
+  try
+  {
+    choice = int.Parse(Console.ReadLine() ?? string.Empty);
+  }
+  catch (System.FormatException)
+  {
+    // Set to invalid value
+    choice = -1;
+  }
+
+  switch(choice)
+  {
+    case 0:
+      // Exit the program
+      Console.WriteLine("Goodbye...");
+      break;
+    case 1:
+      // Display access token
+      await DisplayAccessTokenAsync();
+      break;
+    case 2:
+      // List users
+      await ListUsersAsync();
+      break;
+    case 3:
+      // Run any Graph code
+      await MakeGraphCallAsync();
+      break;
+    default:
+      Console.WriteLine("Invalid choice! Please try again.");
+      break;
+  }
+}
+
+void InitializeGraph(Settings settings)
+{
+  GraphHelper.InitializeGraphForAppOnlyAuth(settings);
+}
+
+async Task DisplayAccessTokenAsync()
+{
+  try
+  {
+    var appOnlyToken = await GraphHelper.GetAppOnlyTokenAsync();
+    Console.WriteLine($"App-only token: {appOnlyToken}");
+  }
+  catch (Exception ex)
+  {
+    Console.WriteLine($"Error getting app-only access token: {ex.Message}");
+  }
+}
+
+
+async Task ListUsersAsync()
+{
+  // TODO
+}
+
+async Task MakeGraphCallAsync()
+{
+  // TODO
+}
+
